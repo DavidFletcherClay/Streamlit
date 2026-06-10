9d32718e1f54cb7e5ea15bca5166b236faf9aa4f
DavidFletcherClay <clayconsultingltd@gmail.com>
2026-06-10 11:40:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,12 @@ st.title("David and his first app. Nerd!!! But he tries")
 # Load the Excel file
 df = pd.read_excel('testing.xlsx')
 
-# Show the dataframe
-#st.subheader("Raw Data")
-#st.dataframe(df)
 
 # Dropdown to select column
 column = st.selectbox("Select:", ['Act', 'Bud', 'PY'])
 
 # Plot the selected column
-st.subheader(f"Line Chart - {column}")
+st.subheader(f"Line Chart shown below - {column}")
 fig, ax = plt.subplots()
 ax.plot(df['Month'], df[column])
 ax.set_xlabel("Month")
@@ -23,3 +20,6 @@ ax.set_ylabel(column)
 plt.xticks(rotation=45)
 
 st.pyplot(fig)
+
+st.subheader("Raw Data")
+st.dataframe(df)
